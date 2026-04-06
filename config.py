4e62a509b01e8f884e91bf85e0dc741e913b1537
lucieDLE <dolelucie0@gmail.com>
2026-04-06 15:48:52 -0400

    change coords and add variables for high res data

--- config.py
+++ config.py
@@ -1,6 +1,12 @@
-DATA_PATH = "data/sst.wkmean.1990-present.nc"
+DATA_PATH = "data/sst.week.mean.nc"
 DATA_REF = "data/sst.oisst.mon.ltm.1991-2020.nc"
 
+
+# Performance tuning for high-res data
+CHUNKS = {"time": 48, "lat": 180, "lon": 180}
+MAP_COARSEN = 2
+RAW_COARSEN = 2
+TIME_COARSEN = 3
 
 MED_BOUNDS = {
     "lat_min": 20.5,
@@ -11,17 +17,27 @@ MED_BOUNDS = {
 
 MIN_YEAR = 1990
 MAX_YEAR = 2022
+
+MIN_DATE="1982-01-01"
+MAX_DATE="2025-12-31"
+
 ROLLING_YEARS = 3
-FREQ_PER_YEAR_MIN = 24
+FREQ_PER_YEAR_MIN = 12
 EXTREME_QUANTILE = 0.95
 
 WIDTH_PLOT=500
 HEIGHT_RIGHT_PLOT = int(WIDTH_PLOT / 3)
 
-DEFAULT_TAP_LON = 40.5
-DEFAULT_TAP_LAT = 40.5
+DEFAULT_TAP_LON = 300
+DEFAULT_TAP_LAT = 40
 
 MAP_WIDTH = 980
 MAP_HEIGHT = 560
 RIGHT_PANEL_WIDTH = 560
 RIGHT_PLOT_HEIGHT = 335
+
+TIME_SERIE_WIDTH = 1440
+TIME_SERIE_HEIGHT = 760
+
+# Cache outputs
+INITIAL_MAP_CACHE = "data/cache/initial_map.nc"
