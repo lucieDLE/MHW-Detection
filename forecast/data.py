b6983e478e71db6ae897bdc61de0fbf10778b96d
lucieDLE <dolelucie0@gmail.com>
2026-04-20 12:19:29 -0400

    fix typo + add coarsening if map too big  to fit memory

--- forecast/data.py
+++ forecast/data.py
@@ -27,17 +27,21 @@ class SSTADataset(Dataset):
         time_range: Tuple[str, str],
         n_in: int = 14,
         n_out: int = 1,
+        coarsen_factor: int = 2,
     ):
         self.n_in = n_in
         self.n_out = n_out
 
         ds = xr.open_zarr(str(ssta_path))
-        ssta = ds.sst.sel(time=slice(*time_range))
+        ds = ds.coarsen(lat=coarsen_factor, lon=coarsen_factor, boundary="trim").mean()
+        
+        ssta = ds.ssta.sel(time=slice(*time_range))
 
         self.ssta = ssta.load().astype("float32").values
         self.times = ssta.time.values
 
-        land_mask = xr.open_zarr(str(landmask_path)).land_mask.values.astype(bool)
+        land_mask = xr.open_zarr(str(landmask_path))
+        land_mask = land_mask.coarsen(lat=coarsen_factor, lon=coarsen_factor, boundary="trim").mean().land_mask.values.astype(bool)
         self.land_mask = land_mask  # (H, W); True over land
 
         np.nan_to_num(self.ssta, copy=False, nan=0.0) # convert the land set to nan to 0 instead
@@ -56,16 +60,17 @@ class SSTADataset(Dataset):
 
 
 def make_loaders(
-    ssta_path: str | Path = config.DL_SSTA_DAILY_PATH,
-    landmask_path: str | Path = config.DL_LANDMASK_PATH,
+    ssta_path: str | Path = config.SSTA_DAILY_PATH,
+    landmask_path: str | Path = config.LANDMASK_PATH,
     n_in: int = config.DL_N_IN,
     n_out: int = config.DL_N_OUT,
-    batch_size: int = config.DL_BATCH_SIZE,
-    num_workers: int = config.DL_NUM_WORKERS,
+    batch_size: int = config.BATCH_SIZE,
+    num_workers: int = config.NUM_WORKERS,
+    coarsen_factor: int = config.DL_COARSEN_FACTOR
 ):
-    train_ds = SSTADataset(ssta_path, landmask_path, config.DL_TRAIN_RANGE, n_in, n_out)
-    val_ds   = SSTADataset(ssta_path, landmask_path, config.DL_VAL_RANGE,   n_in, n_out)
-    test_ds  = SSTADataset(ssta_path, landmask_path, config.DL_TEST_RANGE,  n_in, n_out)
+    train_ds = SSTADataset(ssta_path, landmask_path, config.DL_TRAIN_RANGE, n_in, n_out, coarsen_factor)
+    val_ds   = SSTADataset(ssta_path, landmask_path, config.DL_VAL_RANGE,   n_in, n_out, coarsen_factor)
+    test_ds  = SSTADataset(ssta_path, landmask_path, config.DL_TEST_RANGE,  n_in, n_out, coarsen_factor)
 
     common = dict(num_workers=num_workers, pin_memory=True, persistent_workers=num_workers > 0)
     train_loader = DataLoader(train_ds, batch_size=batch_size, shuffle=True,  drop_last=True,  **common)
